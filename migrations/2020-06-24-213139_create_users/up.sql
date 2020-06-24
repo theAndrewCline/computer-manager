07bee5512a772dcb624b4b37557001131d05227b
Andrew Cline <andrew.cline77@gmail.com>
2020-06-24 16:49:11 -0500

    initial db setup and migration

---
+++ migrations/2020-06-24-213139_create_users/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL
+)
---
+++ src/schema.rs
@@ -0,0 +1,7 @@
+table! {
+    users (id) {
+        id -> Int4,
+        first_name -> Text,
+        last_name -> Text,
+    }
+}

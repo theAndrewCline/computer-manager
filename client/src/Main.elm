d752d538a60cdbe4d9696b20e54f92ffaa21f340
Andrew Cline <andrew.cline77@gmail.com>
2020-06-24 12:13:10 -0500

    add user request and user decoder

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,20 +1,52 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, div, h1, img, li, text, ul)
 import Html.Attributes exposing (src)
+import Http
+import Json.Decode as Decode exposing (field, int, list, map3, string)
+
+
+
+---- USER ----
+
+
+type alias User =
+    { first_name : String
+    , last_name : String
+    , id : Int
+    }
+
+
+user_decoder =
+    map3 User
+        (field "first_name" string)
+        (field "first_name" string)
+        (field "id" int)
+
+
+user_list_decoder =
+    list user_decoder
+
+
+get_users =
+    Http.get
+        { url = "localhost:8000/users"
+        , expect = Http.expectJson GotUsers user_list_decoder
+        }
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { users : List User }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { users = [] }, get_users )
 
 
 
@@ -22,12 +54,23 @@ init =
 
 
 type Msg
-    = NoOp
+    = GotUsers (Result Http.Error (List User))
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        GotUsers result ->
+            case result of
+                Ok users ->
+                    ( { model | users = users }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
 
 
 
@@ -37,8 +80,13 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ img [ src "/logo.svg" ]
+            []
+        , ul []
+            (List.map
+                (\user -> li [] [ text user.first_name, text user.last_name ])
+                model.users
+            )
         ]
 
 

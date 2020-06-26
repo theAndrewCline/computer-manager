9d525fd428d1607177f261eda2183e19935571bc
Andrew Cline <andrew.cline77@gmail.com>
2020-06-26 10:06:19 -0500

    remove files related to db

--- migrations/2020-06-24-213139_create_users/up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY AUTOINCREMENT,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL
-)
--- src/schema.rs
+++
@@ -1,7 +0,0 @@
-table! {
-    users (id) {
-        id -> Int4,
-        first_name -> Text,
-        last_name -> Text,
-    }
-}

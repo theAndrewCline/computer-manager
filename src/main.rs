f3eaed45f8f5616abe525b90f78280ed8910e421
Andrew Cline <andrew.cline77@gmail.com>
2020-06-26 10:10:39 -0500

    refactor cors to be a function

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,30 @@
 use rocket;
 use rocket_cors;
 
-mod user;
-
 use rocket::http::Method;
 use rocket_contrib::serve::StaticFiles;
 use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
+mod user;
+
 fn main() {
+    rocket::ignite()
+        .mount("/", StaticFiles::from("client/build"))
+        .mount("/users", user::routes())
+        .attach(cors())
+        .launch();
+}
+
+fn cors() -> rocket_cors::Cors {
     let (allowed_origins, failed_origins) =
         AllowedOrigins::some(&["http://localhost:3000", "http://localhost:8000"]);
     assert!(failed_origins.is_empty());
 
-    // You can also deserialize this
-    let options = rocket_cors::Cors {
+    rocket_cors::Cors {
         allowed_origins: allowed_origins,
         allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
         allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
         allow_credentials: true,
         ..Default::default()
-    };
-
-    rocket::ignite()
-        .mount("/", StaticFiles::from("client/build"))
-        .mount("/users", user::routes())
-        .attach(options)
-        .launch();
+    }
 }

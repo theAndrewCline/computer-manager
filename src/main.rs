d30d8ac7ee0ea34110a1ee72c2edfb8cd1443ded
Andrew Cline <andrew.cline77@gmail.com>
2020-07-01 16:16:10 -0500

    add count route

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@ use rocket;
 use rocket_cors;
 
 use rocket::http::Method;
+use rocket::{get, routes, State};
 use rocket_contrib::serve::StaticFiles;
 use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 mod user;
 
+#[get("/")]
+fn hit_count(count: State<u32>) -> String {
+    format!("count {}", count.inner())
+}
+
 fn main() {
     rocket::ignite()
+        .manage(0)
         .mount("/", StaticFiles::from("client/build"))
+        .mount("/count", routes![hit_count])
         .mount("/users", user::routes())
         .attach(cors())
         .launch();
